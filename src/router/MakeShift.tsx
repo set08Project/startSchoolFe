1414e7d5a35c7a1482084ad6b2384d859ccc8d57
PetxCode <brighterdayscodelab@gmail.com>
2024-03-16 18:22:43 +0100

    suspense error fixed on all router

--- src/router/MakeShift.tsx
+++ src/router/MakeShift.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import React from "react";
+
 import { getSchoolCookie, readSchool } from "../pages/api/schoolAPIs";
 import { Outlet } from "react-router-dom";
 const LoadingScreen = React.lazy(
