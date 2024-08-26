c78673351e99cc26a8967a5e805bd0927173c7d3
unknown <zubby0013>
2024-08-26 18:21:14 +0100

    env URL

--- src/pages/api/schoolAPIs.tsx
+++ src/pages/api/schoolAPIs.tsx
@@ -1,27 +1,9 @@
 import axios from "axios";
 
-// working locally
-
-const URL2: string = import.meta.env.VITE_URL;
-const URL: string = import.meta.env.VITE_MAIN_URL;
-
-// working locally
-
-// const URL2: string = import.meta.env.VITE_PROCUTION_URL2;
-// const URL: string = import.meta.env.VITE_PROCUTION_URL;
-
-// working remotely
-
-// const URL: string = import.meta.env.VITE_PROCUTION_URL;
-// const URL2: string = import.meta.env.VITE_PROCUTION_URL2;
-
-// working remotely 1
-
-// const URL: string = "https://startschoolbe.onrender.com/api";
-// const URL2: string = "https://startschoolbe.onrender.com";
-
-// const URL: string = "https://startschool.onrender.com/api";
-// const URL2: string = "https://startschool.onrender.com";
+const URL2: string =
+  import.meta.env.VITE_URL || import.meta.env.VITE_PRODUCTION_URL2;
+const URL: string =
+  import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
 export const deleteStudent = async (schoolID: string, studentID: string) => {
   try {
