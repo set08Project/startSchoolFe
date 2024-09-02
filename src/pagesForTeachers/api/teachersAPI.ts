25ceedf3801bfaa5515bf315c73407fab60caa69
rosonerri <gabrieldotajayi4real@gmail.com>
2024-09-03 00:10:08 +0100

    Typographical error fixed

--- src/pagesForTeachers/api/teachersAPI.ts
+++ src/pagesForTeachers/api/teachersAPI.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-
 // const URL: string = import.meta.env.VITE_MAIN_URL;
 
 // const URL: string = import.meta.env.VITE_MAIN_URL;
@@ -18,11 +17,10 @@ const URL: string = "https://startschool.onrender.com/api";
 // const URL: string =
 //   import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
-
 // const URL: string =
 //   import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
-const URL: string = "https://startschoolbe.onrender.com/api";
+// const URL: string = "https://startschoolbe.onrender.com/api";
 
 export const viewTeacherDetail: any = async (teacherID: string) => {
   try {
