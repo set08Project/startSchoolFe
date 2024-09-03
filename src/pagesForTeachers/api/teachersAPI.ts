bd2d830582401eec02bc1e812951893a8f54f552
rosonerri <gabrieldotajayi4real@gmail.com>
2024-09-04 00:58:37 +0100

    jfjfjf

--- src/pagesForTeachers/api/teachersAPI.ts
+++ src/pagesForTeachers/api/teachersAPI.ts
@@ -7,7 +7,7 @@ import axios from "axios";
 
 // const URL: string = "https://startschoolbe.onrender.com/api";
 
-const URL: string = "https://startschool.onrender.com/api";
+// const URL: string = "https://startschool.onrender.com/api";
 // const URL: string = "https://startschool.onrender.com/api";
 // const URL: string = "https://startschool.onrender.com/api";
 
@@ -20,7 +20,7 @@ const URL: string = "https://startschool.onrender.com/api";
 // const URL: string =
 //   import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
-// const URL: string = "https://startschoolbe.onrender.com/api";
+const URL: string = "https://startschoolbe.onrender.com/api";
 
 export const viewTeacherDetail: any = async (teacherID: string) => {
   try {
