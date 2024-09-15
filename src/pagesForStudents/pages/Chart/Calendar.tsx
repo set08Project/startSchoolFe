37c03dc483511772c3ab4269a406d07b089f889e
Rosonerri <gabrieldotajayi4real@gmail.com>
2024-09-15 08:12:33 +0100

    Calender Adjusted on Mobile and Desktop

--- src/pagesForStudents/pages/Chart/Calendar.tsx
+++ src/pagesForStudents/pages/Chart/Calendar.tsx
@@ -17,8 +17,8 @@ const Calendar = () => {
     <div className="flex flex-col items-center mr-10 mb-12">
       <div className="my-4 text-[25px] text-blue-950 font-bold">Calendar</div>
       <div className="w-[80%] text-blue-950  mb-5 flex justify-center items-center gap-2">
-        <div className="text-blue-950  font-semibold">{currentDay},</div>
-        <div className="text-blue-950  font-semibold">{currentMonth} 2024</div>
+        <div className="text-blue-950 font-semibold text-[13px] md:text-[17px]">{currentDay},</div>
+        <div className="text-blue-950  font-semibold text-[13px] md:text-[17px]">{currentMonth} 2024</div>
       </div>
       <div className="w-full grid grid-cols-7 gap-5">
         {data.map((props) => (
@@ -31,7 +31,7 @@ const Calendar = () => {
             key={day}
             className={`flex items-center justify-center py-2 px-3 rounded-full ${
               day === currentDate.getDate()
-                ? "bg-indigo-800 border-[2px]  border-white text-white"
+                ? "bg-indigo-800 border-[2px]  border-white text-white rounded-[50%] h-[35px] w-[35px]"
                 : "bg-white"
             }`}
           >
