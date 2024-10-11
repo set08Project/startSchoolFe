b982afa31f973270f3615310e251abbe7d489090
dwaynissh <dwaynissh@gmail.com>
2024-10-11 01:03:04 +0100

    all

--- src/pagesForStudents/static/SmallPiece.tsx
+++ src/pagesForStudents/static/SmallPiece.tsx
@@ -84,8 +84,8 @@ const SmallPiece: FC<iProps> = ({ log, name, but }) => {
   };
 
   return (
-    <div className="border w-[250px] h-[550px] bg-blue-50 shadow-sm  rounded-md p-1 overflow-y-auto  z-50">
-      <div className="flex flex-col items-between  w-full">
+    <div className="border w-[250px] bg-blue-50 shadow-sm  rounded-md p-1 overflow-y-auto  z-50">
+      <div className="flex flex-col items-between w-full">
         {name?.map(({ title, icon, to }, i: number) => (
           <NavLink
             key={i}
