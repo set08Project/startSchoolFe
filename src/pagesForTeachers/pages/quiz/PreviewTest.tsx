13a0de3b1d67bd0195354465b00392944a3e9541
dwaynissh <dwaynissh@gmail.com>
2024-11-01 23:54:54 +0100

    ...

--- src/pagesForTeachers/pages/quiz/PreviewTest.tsx
+++ src/pagesForTeachers/pages/quiz/PreviewTest.tsx
@@ -32,7 +32,7 @@ const PreviewTest = () => {
   return (
     <div>
       <p>Preview Test Entries</p>
-      <p className="mt-10 font-bold">Test Instruction</p>
+      <p className="mt-10 font-bold">Test Instructions</p>
       <p className="mr-6 mt-2">{testQuestion[0]?.instruction?.instruction}</p>
       <div className="flex gap-3 capitalize mt-5 font-medium text-[14px] mr-6 pb-5 border-b">
         <p>duration: {testQuestion[0]?.instruction?.duration}hour</p>
@@ -48,7 +48,7 @@ const PreviewTest = () => {
 
               <div className="ml-8">
                 <p className="text-[15px] mt-5 mb-3 italic">
-                  Answer: {props.answer}
+                  Answer: {props?.answer}
                 </p>
                 {props.options.map((props: any, i: number) => {
                   const choiceValue = Object.values(props)[0];
@@ -94,7 +94,6 @@ const PreviewTest = () => {
             console.log(res);
             if (res.status === 201) {
               mutate(`api/view-subject-quiz/${subjectID}`);
-
               dispatch(displayEmptyTest());
 
               navigate(`/subjects/${subjectID}`);
