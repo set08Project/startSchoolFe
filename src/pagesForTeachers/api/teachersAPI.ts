bf39c6d522d9d9d185cc76d902efc0ed6a91ca37
petxcode <brighterdayscodelab@gmail.com>
2024-11-19 06:06:38 +0100

    fixed

--- src/pagesForTeachers/api/teachersAPI.ts
+++ src/pagesForTeachers/api/teachersAPI.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-// // const URL: string =
+// const URL: string =
 // import.meta.env.VITE_MAIN_URL || import.meta.env.VITE_PRODUCTION_URL;
 
 // const URL: string = "https://startschoolbe-1.onrender.com/api";
