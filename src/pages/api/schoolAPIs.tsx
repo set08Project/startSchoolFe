d38501a9c7c95951d8b7b6958ceeca33de8377b7
petxcode <brighterdayscodelab@gmail.com>
2025-05-05 02:13:09 +0100

    update API

--- src/pages/api/schoolAPIs.tsx
+++ src/pages/api/schoolAPIs.tsx
@@ -43,7 +43,7 @@ export const deleteStudentHistory = async (
   resultID: string
 ): Promise<any> => {
   try {
-    const URL = "http://localhost:2244/api";
+   
     return await axios
       .delete(`${URL}/delete-student-result-history/${studentID}/${resultID}`)
       .then((res: AxiosResponse) => {
